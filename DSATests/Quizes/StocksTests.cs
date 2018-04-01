5019f2cb8f8bfec5dc0e8a2ec3877d8e03601f04
Rahul Vats <vats.rahul@gmail.com>
2018-03-31 23:00:48 -0400

    One Final Test For Stocks Problem and it worked, my solution was working for all situations and it was faster for most unit test cases that were run.

--- DSATests/Quizes/StocksTests.cs
+++ DSATests/Quizes/StocksTests.cs
@@ -13,6 +13,7 @@ namespace StocksTests
         public int[] stockPricesYesterdayCase1;
         public int[] stockPricesYesterdayCase2;
         public int[] stockPricesYesterdayCase3;
+        public int[] stockPricesYesterdayCase4;
 
         [TestInitialize]
         public void StocksData()
@@ -20,6 +21,7 @@ namespace StocksTests
             stockPricesYesterdayCase1 = new int[] { 10, 7, 5, 8, 11, 9 };
             stockPricesYesterdayCase2 = new int[] { 5, 4, 3, 2, 1 };
             stockPricesYesterdayCase3 = new int[] { 10 };
+            stockPricesYesterdayCase4 = new int[] { 100, 200, 150, 50, 10, 90, 50, 100, 200, 300, 400, 50, 10, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 900, 300, 100, 50, 25, 30, 20, 10 };
         }
 
         [TestMethod]
@@ -98,6 +100,32 @@ namespace StocksTests
             var maxProfit = Stocks.GetMaxProfitAlternate(stockPricesYesterdayCase3);
 
             // Assert
+        }
+
+        [TestMethod]
+        public void TestGetMaxProfitCase4()
+        {
+            // Arrange
+            StocksData();
+
+            // Act
+            var maxProfit = Stocks.GetMaxProfit(stockPricesYesterdayCase4);
+
+            // Assert
+            Assert.AreEqual(990, maxProfit, "Incorrect Profit is being returned");
+        }
+
+        [TestMethod]
+        public void TestGetMaxProfitAlternateCase4()
+        {
+            // Arrange
+            StocksData();
+
+            // Act
+            var maxProfit = Stocks.GetMaxProfitAlternate(stockPricesYesterdayCase4);
+
+            // Assert
+            Assert.AreEqual(990, maxProfit, "Incorrect Profit is being returned");
         }
     }
 }
