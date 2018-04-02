e39a19a94ac8316c87aa5e299dc07493f36e087c
Rahul Vats <vats.rahul@gmail.com>
2018-04-02 17:36:46 -0400

    Commiting Merge Meeting Tests

--- DSATests/Quizes/GeneralTests.cs
+++ DSATests/Quizes/GeneralTests.cs
@@ -9,7 +9,7 @@ namespace DSATests
         public static int[] Nums1 = { 2, 7, 11, 15, 1, 4, 3, 5, 6, 8, 9, 10 };
         public static int[] Nums2 = { -1, 0, -2, 0, 1, 2, -1, -2, 1, 2 };
         public static int[] arrayForProducts = { 1, 7, 3, 4, -10, -10 };
-        public static List<Meeting> allMeetings = new List<Meeting>();
+        
         public static int[] changeDenominations = { 1, 2, 3 };
         public static string[] words = new string[]
         {
@@ -79,25 +79,6 @@ namespace DSATests
         public static void TestChangeDenominations()
         {
             Console.WriteLine("Ways to Create Amount from Given Denominations: {0}", ArrayExtensions.ChangePossibilitiesBottomUp(1, changeDenominations));
-        }
-        public static void TestMergingMeetings()
-        {
-            allMeetings.Add(new Meeting(0, 1));
-            allMeetings.Add(new Meeting(3, 5));
-            allMeetings.Add(new Meeting(4, 8));
-            allMeetings.Add(new Meeting(10, 12));
-            allMeetings.Add(new Meeting(9, 10));
-            Console.WriteLine("All Meetings before Merging");
-            foreach (var meeting in allMeetings)
-            {
-                Console.WriteLine(meeting);
-            }
-            Console.WriteLine("All Meetings after Merging");
-            var mergeMeetings = Meeting.MergeRanges(allMeetings);
-            foreach (var meeting in mergeMeetings)
-            {
-                Console.WriteLine(meeting);
-            }
         }
 
         public static void TestHighestProductFrom3NumbersInArray()
