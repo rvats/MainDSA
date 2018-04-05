413a17fd0fff6c5801bc6d9b87454251ef355b82
Rahul Vats <vats.rahul@gmail.com>
2018-04-04 20:25:02 -0400

    Adding LeetCode Quizes

---
+++ Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using MainDSA.Quizes;
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static string[] cpDomains = new string[] { "10000 google.com", "50 yahoo.com" };
+
+        static void Main(string[] args)
+        {
+            foreach (var subDomainVisit in DomainVisits.SubdomainVisits(cpDomains))
+            {
+                Console.WriteLine(subDomainVisit);
+            }
+        }
+    }
+}
