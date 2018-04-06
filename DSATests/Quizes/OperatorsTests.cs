d81c32a78ed69b987ba50ce044c80e3fbe06c668
Rahul Vats <vats.rahul@gmail.com>
2018-04-06 07:45:03 -0400

    Solution For https://leetcode.com/problems/expression-add-operators/description/

---
+++ DSATests/Quizes/OperatorsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MainDSA.Quizes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DSATests.Quizes
+{
+    [TestClass]
+    public class OperatorsTests
+    {
+        [TestMethod]
+        public void TestAddOperators1()
+        {
+            // Arrange and Act
+            var expressions = Operators.AddOperators("123", 6);
+            // Assert
+            Assert.AreEqual(2, expressions.Count, "Wrong Expression Count");
+            Assert.AreEqual("1+2+3", expressions[0], "Wrong Expression");
+            Assert.AreEqual("1*2*3", expressions[1], "Wrong Expression");
+        }
+    }
+}
