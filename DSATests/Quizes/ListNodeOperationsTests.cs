bf963ced82e46be175a174738c808d3b9021b58e
Rahul Vats <vats.rahul@gmail.com>
2018-04-06 11:49:48 -0400

    Added Solution for Reversing A List https://leetcode.com/explore/interview/card/facebook/6/round-2-onsite-interview/318/

---
+++ DSATests/Quizes/ListNodeOperationsTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MainDSA.DataStructures.Lists;
+using MainDSA.Quizes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DSATests.Quizes
+{
+    [TestClass]
+    public class ListNodeOperationsTests
+    {
+        public ListNode head;
+        [TestInitialize]
+        public void CreateTestData()
+        {
+            // Create Head
+            head = new ListNode(0);
+            
+            // Create Next Node
+            ListNode next1 = new ListNode(1);
+            head.Next = next1;
+
+            // Create Next Node
+            ListNode next2 = new ListNode(2);
+            next1.Next = next2;
+
+            // Create End Node
+            ListNode end = new ListNode(3);
+            next2.Next = end;
+        }
+
+        [TestMethod]
+        public void TestReverseListIteratively1()
+        {
+            // Arrange
+            CreateTestData();
+
+            // Act
+            var listNodeOperations = new ListNodeOperations();
+            var reverseHead = listNodeOperations.ReverseListIteratively(head);
+
+            // Assert
+            Assert.AreEqual(3, reverseHead.Value, "Wrong Value");
+            Assert.AreEqual(2, reverseHead.Next.Value, "Wrong Value");
+            Assert.AreEqual(1, reverseHead.Next.Next.Value, "Wrong Value");
+            Assert.AreEqual(0, reverseHead.Next.Next.Next.Value, "Wrong Value");
+        }
+        
+        [TestMethod]
+        public void TestReverseListRecursively1()
+        {
+            // Arrange
+            CreateTestData();
+
+            // Act
+            var listNodeOperations = new ListNodeOperations();
+            var reverseHead = listNodeOperations.ReverseListRecursively(head);
+
+            // Assert
+            Assert.AreEqual(3, reverseHead.Value, "Wrong Value");
+            Assert.AreEqual(2, reverseHead.Next.Value, "Wrong Value");
+            Assert.AreEqual(1, reverseHead.Next.Next.Value, "Wrong Value");
+            Assert.AreEqual(0, reverseHead.Next.Next.Next.Value, "Wrong Value");
+        }
+    }
+}
---
+++ Quizes/ListNodeOperations.cs
@@ -0,0 +1,35 @@
+﻿using MainDSA.DataStructures.Lists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainDSA.Quizes
+{
+    public class ListNodeOperations
+    {
+        public ListNode ReverseListIteratively(ListNode head)
+        {
+            ListNode previousNode = null;
+            ListNode currentNode = head;
+            while (currentNode != null)
+            {
+                ListNode tempNode = currentNode.Next;
+                currentNode.Next = previousNode;
+                previousNode = currentNode;
+                currentNode = tempNode;
+            }
+            return previousNode;
+        }
+
+        public ListNode ReverseListRecursively(ListNode head)
+        {
+            if (head == null || head.Next == null) return head;
+            ListNode previous = ReverseListRecursively(head.Next);
+            head.Next.Next = head;
+            head.Next = null;
+            return previous;
+        }
+    }
+}
