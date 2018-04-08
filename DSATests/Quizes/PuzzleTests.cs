19cf0b1cf08e5ec88cea5b8180166142e0c5a15e
Rahul Vats <vats.rahul@gmail.com>
2018-04-08 19:23:04 -0400

    Added Tests for Number To English.

---
+++ DSATests/Quizes/PuzzleTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MainDSA.Quizes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DSATests.Quizes
+{
+    [TestClass]
+    public class PuzzleTests
+    {
+        [TestMethod]
+        public void TestNumberToWords()
+        {
+            var puzzle = new Puzzles();
+            var numberInEnglish = puzzle.NumberToWords(987654321);
+            Assert.AreEqual("Nine Hundred Eighty Seven Million Six Hundred Fifty Four Thousand Three Hundred Twenty One", numberInEnglish, "Wrong Value");
+            puzzle = new Puzzles();
+            numberInEnglish = puzzle.NumberToWords(123467890);
+            Assert.AreEqual("One Hundred Twenty Three Million Four Hundred Sixty Seven Thousand Eight Hundred Ninety", numberInEnglish, "Wrong Value");
+        }
+    }
+}
