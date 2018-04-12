623af1ff6d7136ea2a69502f2308743c73f68f5f
Rahul Vats <vats.rahul@gmail.com>
2018-04-12 13:04:19 -0400

    Solution To Merge Sorted Arrays https://leetcode.com/explore/interview/card/facebook/54/sorting-and-searching-3/309/

--- Quizes/ArrayExtensions.cs
+++ Quizes/ArrayExtensions.cs
@@ -5,6 +5,29 @@ namespace MainDSA.Quizes
 {
     public static class ArrayExtensions
     {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            while (m > 0 && n > 0)
+            {
+                if (nums1[m - 1] > nums2[n - 1])
+                {
+                    nums1[m + n - 1] = nums1[m - 1];
+                    m--;
+                }
+                else
+                {
+                    nums1[m + n - 1] = nums2[n - 1];
+                    n--;
+                }
+            }
+
+            while (n > 0)
+            {
+                nums1[m + n - 1] = nums2[n - 1];
+                n--;
+            }
+        }
+
         public static int NumIslands(char[,] grid)
         {
             if (grid == null || grid.Length == 0 || grid.GetUpperBound(0) < 0 || grid.GetUpperBound(1) < 0)
