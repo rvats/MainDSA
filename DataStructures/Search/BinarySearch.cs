6538034e674e66b9988d265297ddc9a567574528
Rahul Vats <vats.rahul@gmail.com>
2018-04-12 14:22:14 -0400

    Search in Rotated Sorted Array II https://leetcode.com/explore/interview/card/facebook/54/sorting-and-searching-3/284/

--- DataStructures/Search/BinarySearch.cs
+++ DataStructures/Search/BinarySearch.cs
@@ -2,6 +2,48 @@
 {
     public class BinarySearch
     {
+        public bool SearchForN(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                    return true;
+
+                if (nums[left] < nums[mid])
+                {
+                    if (nums[left] <= target && target < nums[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else if (nums[left] > nums[mid])
+                {
+                    if (nums[mid] < target && target <= nums[right])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                else
+                {
+                    left++;
+                }
+            }
+
+            return false;
+        }
+
         public int Search(int[] nums, int target)
         {
             return Search(nums, 0, nums.Length - 1, target);
