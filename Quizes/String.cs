1dea9c6f984f5a89105b6d8c7a1878bb0177b383
Rahul Vats <vats.rahul@gmail.com>
2018-04-12 23:00:43 -0400

    Solution To https://leetcode.com/explore/interview/card/facebook/53/recursion-3/307/

--- Quizes/String.cs
+++ Quizes/String.cs
@@ -6,6 +6,34 @@ namespace MainDSA.Quizes
 {
     public static class String
     {
+        public static bool IsRegexMatch(string s, string p)
+        {
+            var dp = new bool[s.Length + 1, p.Length + 1];
+            dp[0, 0] = true;
+            for (int i = 1; i < p.Length; i++)
+            {
+                if (p[i] == '*') dp[0, i + 1] = dp[0, i - 1];
+                else dp[0, i + 1] = false;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < p.Length; j++)
+                {
+                    if (s[i] == p[j] || p[j] == '.')
+                    {
+                        dp[i + 1, j + 1] = dp[i, j];
+                    }
+                    else if (p[j] == '*')
+                    {
+                        dp[i + 1, j + 1] = dp[i + 1, j - 1] || (dp[i, j + 1] && (s[i] == p[j - 1] || p[j - 1] == '.'));
+                    }
+                }
+            }
+
+            return dp[s.Length, p.Length];
+        }
+
         public static bool IsMatch(string s, string p)
         {
             var dp = new bool[s.Length + 1, p.Length + 1];
