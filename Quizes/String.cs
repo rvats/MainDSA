dc5fb92639dd6fcd8819938c1b045222a11ae716
Rahul Vats <vats.rahul@gmail.com>
2018-04-12 22:10:15 -0400

    Solution to Wildcard Matching https://leetcode.com/explore/interview/card/facebook/53/recursion-3/294/ https://www.programcreek.com/2014/05/leetcode-remove-invalid-parentheses-java/

--- Quizes/String.cs
+++ Quizes/String.cs
@@ -6,6 +6,81 @@ namespace MainDSA.Quizes
 {
     public static class String
     {
+        public static bool IsMatch(string s, string p)
+        {
+            var dp = new bool[s.Length + 1, p.Length + 1];
+            dp[0, 0] = true;
+
+            for (int j = 0; j < p.Length; j++)
+            {
+                if (p[j] == '*')
+                {
+                    dp[0, j + 1] = dp[0, j];
+                }
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < p.Length; j++)
+                {
+                    if (s[i] == p[j])
+                    {
+                        dp[i + 1, j + 1] = dp[i, j];
+                    }
+                    else
+                    {
+                        if (p[j] == '?')
+                        {
+                            dp[i + 1, j + 1] = dp[i, j];
+                        }
+                        else if (p[j] == '*')
+                        {
+                            dp[i + 1, j + 1] = dp[i + 1, j] || dp[i, j + 1];
+                        }
+                        else
+                        {
+                            dp[i + 1, j + 1] = false;
+                        }
+                    }
+                }
+            }
+
+            return dp[s.Length, p.Length];
+        }
+
+        public static bool IsMatchSlow(string s, string p)
+        {
+            return IsMatchRecursion(s, 0, p, 0);
+        }
+
+        private static bool IsMatchRecursion(string s, int sindex, string p, int pindex)
+        {
+            while (sindex >= s.Length && pindex < p.Length && p[pindex] == '*')
+            {
+                pindex++;
+            }
+            if (sindex >= s.Length && pindex >= p.Length)
+            {
+                return true;
+            }
+
+            if (sindex >= s.Length || pindex >= p.Length)
+            {
+                return false;
+            }
+            if (p[pindex] == '*')
+            {
+                return IsMatchRecursion(s, sindex + 1, p, pindex) || IsMatchRecursion(s, sindex, p, pindex + 1);
+            }
+
+            if (p[pindex] == '?' || p[pindex] == s[sindex])
+            {
+                return IsMatchRecursion(s, sindex + 1, p, pindex + 1);
+            }
+
+            return false;
+        }
+
         public static string MinWindow(string stringToSearchIn, string pattern)
         {
             int lengthStringToSearchIn = stringToSearchIn.Length;
