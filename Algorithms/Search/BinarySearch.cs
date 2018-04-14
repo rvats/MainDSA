3001bf2a5f9459a3c610b3ef91a9b55a1eb6cb83
Rahul Vats <vats.rahul@gmail.com>
2018-04-13 22:12:42 -0400

    Solution To Finding Lowest Greater Character from The Target Character in  a given sorted array.

--- Algorithms/Search/BinarySearch.cs
+++ Algorithms/Search/BinarySearch.cs
@@ -1,13 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MainDSA.Algorithms.Search
 {
     public class BinarySearch
     {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            letters = letters.Distinct().ToArray();
+            int first = 0;
+            int last = letters.Length - 1;
+            int mid = 0;
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+                if (letters[mid] == target)
+                {
+                    break;
+                }
+                else if (letters[mid] < target) first = mid + 1;
+                else last = mid - 1;
+            }
+            if(target < letters[mid])
+            {
+                return letters[mid];
+            }
+            else
+            {
+                if (mid == letters.Length - 1)
+                {
+                    return letters[0];
+                }
+                return letters[mid + 1];
+            }
+            //if (mid >= 0)
+            //{
+            //    if (mid + 1 >= letters.Length)
+            //        return letters[0];
+            //    else
+            //        return letters[mid + 1];
+            //}
+            //else
+            //{
+            //    mid = -mid - 1;
+            //    if (mid == letters.Length)
+            //        return letters[0];
+            //    else
+            //        return letters[mid];
+            //}
+        }
+
         public int SearchIteratively(int[] nums, int target)
         {
             int first = 0;
